22752b663bc28056d1eff438fba57d88a025e691
kiyarashnd <kiarashnadri@yahoo.com>
2023-11-10 22:20:05 +0330

    web server

--- server.js
+++ server.js
@@ -1,144 +1,120 @@
-/*
-// console.log(global);
-
-const os = require('os');
-// const path = require('path');
-// const math = require('./math');
-const { add, subtract, multiply, divide } = require('./math');
-
-// console.log(math.add(2, 3));
-// console.log(add(2, 3));
-// console.log(subtract(2, 3));
-// console.log(multiply(2, 3));
-// console.log(divide(2, 3));
-
-// console.log(os.type());
-// console.log(os.version());
-// console.log(os.homedir());
-
-// console.log(__dirname);
-// console.log(__filename);
-
-// console.log(path.dirname(__filename));
-// console.log(path.basename(__filename));
-// console.log(path.extname(__filename));
-
-// console.log(path.parse(__filename));
-
+const http = require('http');
+const path = require('path');
 const fs = require('fs');
-// fs.readFile('./file/starter.txt', 'utf-8', (err, data) => {
-//   if (err) throw err;
-//   //   console.log(data.toString());
-//   console.log(data);
-// });
+const fsPromises = require('fs').promises;
 
-const path = require('path');
+const logEvents = require('./logEvent');
+const EventEmitter = require('events');
 
-fs.readFile(
-  path.join(__dirname, 'file', 'starter.txt'),
-  'utf-8',
-  (err, data) => {
-    if (err) throw err;
-    //   console.log(data.toString());
-    console.log(data);
-  }
-);
+class Emitter extends EventEmitter {}
 
-console.log('Hello...');
+const myEmitter = new Emitter();
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
 
-//exit on uncaught errors,
-process.on('uncaughtException', (err) => {
-  console.error(`There was an uncaught error : ${err}`);
-  process.exit(1);
-});
+const PORT = process.env.PORT || 3500;
 
-fs.writeFile(
-  path.join(__dirname, 'file', 'reply.txt'),
-  //   'utf-8',
-  'Nice to meet you',
-  (err) => {
-    if (err) throw err;
-    //   console.log(data.toString());
-    console.log('write compelete');
-
-    fs.appendFile(
-      path.join(__dirname, 'file', 'reply.txt'),
-      //   'utf-8',
-      '\n\nYes it is ',
-      (err) => {
-        if (err) throw err;
-        console.log('Append compelete');
-
-        fs.rename(
-          path.join(__dirname, 'file', 'reply.txt'),
-          //   'utf-8',
-          path.join(__dirname, 'file', 'new.txt'),
-          (err) => {
-            if (err) throw err;
-            console.log('Rename compelete');
-          }
-        );
-      }
-    );
-  }
-);
-
-// fs.appendFile(
-//   path.join(__dirname, 'file', 'test.txt'),
-//   //   'utf-8',
-//   'Testing text',
-//   (err) => {
-//     if (err) throw err;
-//     //   console.log(data.toString());
-//     console.log('Append compelete');
-//   }
-// );
-
-*/
-//how to prevent from callback hell :
-const fsPromise = require('fs').promises;
-const path = require('path');
-
-const fileOps = async () => {
+const serveFile = async (filePath, contentType, response) => {
   try {
-    const data = await fsPromise.readFile(
-      path.join(__dirname, 'file', 'starter.txt'),
-      'utf-8',
-      console.log('1-read file')
+    const rawData = await fsPromises.readFile(
+      filePath,
+      !contentType.includes('image') ? 'utf8' : ''
     );
-    console.log('out of first ');
+    const data =
+      contentType === 'application/json' ? JSON.parse(rawData) : rawData;
 
-    await fsPromise.unlink(
-      path.join(__dirname, 'file', 'starter.txt'),
-      console.log('2-delete file')
-    ); //unlink for delete file
-
-    await fsPromise.writeFile(
-      path.join(__dirname, 'file', 'promiseWrite.txt'),
-      data,
-      console.log('3-write file')
+    // const data = await fsPromises.readFile(filePath, 'utf8');
+    response.writeHead(filePath.includes('404.html') ? 404 : 200, {
+      'Content-Type': contentType,
+    });
+    response.end(
+      contentType === 'application/json' ? JSON.stringify(data) : data
     );
-
-    await fsPromise.appendFile(
-      path.join(__dirname, 'file', 'promiseWrite.txt'),
-      '\n\nNice to meet you',
-      console.log('4-append file')
-    );
-
-    await fsPromise.rename(
-      path.join(__dirname, 'file', 'promiseWrite.txt'),
-      path.join(__dirname, 'file', 'promiseComplete.txt'),
-      console.log('5-rename file ')
-    );
-
-    const newData = await fsPromise.readFile(
-      path.join(__dirname, 'file', 'promiseComplete.txt'),
-      'utf-8'
-    );
-    console.log('after latest function');
   } catch (err) {
     console.log(err);
+    myEmitter.emit('log', `${err.name} : ${err.message}`, 'errLog.txt');
+
+    response.statusCode = 500;
+    response.end();
   }
 };
 
-fileOps();
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+
+  const extension = path.extname(req.url);
+
+  let contentType;
+
+  switch (extension) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.jpg':
+      contentType = 'image/jpeg';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.txt':
+      contentType = 'text/plain';
+      break;
+
+    default:
+      contentType = 'text/html';
+      break;
+  }
+
+  let filePath =
+    contentType === 'text/html' && req.url === '/'
+      ? path.join(__dirname, 'views', 'index.html')
+      : contentType === 'text/html' && req.url.slice(-1) === '/'
+      ? path.join(__dirname, 'views', req.url, 'index.html')
+      : contentType === 'text/html'
+      ? path.join(__dirname, 'views', req.url)
+      : path.join(__dirname, req.url);
+
+  //makes the .html extension not requrired in the browser
+  if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    //serve the file
+    serveFile(filePath, contentType, res);
+  } else {
+    //404
+    //301
+    // console.log(path.parse(filePath));
+    switch (path.parse(filePath).base) {
+      case 'old-page.html':
+        res.writeHead(301, { Location: '/new-page.html' });
+        res.end();
+        break;
+      case 'www-page.html':
+        res.writeHead(301, { Location: '/' });
+        res.end();
+        break;
+      default:
+        //serve a 404 response
+        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+    }
+  }
+  //   let filePath;
+  //   switch (req.url) {
+  //     case '/':
+  //       res.statusCode = 200;
+  //       filePath = filePath.join(__dirname, 'views', 'index.html');
+  //       fs.readFile(path, 'utf8', (err, data) => {
+  //         res.end(data);
+  //       });
+  //       break;
+  //   }
+});
+server.listen(PORT, () => console.log(`Server running on prot ${PORT}`));
